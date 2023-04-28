438f6820491cad2690302277c98419dc4ad8326e
Kimjisung144 <48574631+Kimjisung144@users.noreply.github.com>
2023-04-28 19:20:04 +0900

    Create P0000.cpp

---
+++ home_work/P0000.cpp
@@ -0,0 +1,20 @@
+#include "P0000.h"
+#include <string.h>
+using namespace std;
+P0000 :: P0000() {
+
+}
+P0000 :: P0000(int r) {
+
+}
+P0000 :: ~P0000() {
+
+}
+void P0000 :: getter() {
+
+}
+void P0000 :: setter(string user_name, string user_id, int user_age) {
+	name = nuser_name;
+	id = user_id;
+	age = user_age;
+}
